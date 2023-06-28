ee50460a4d89965928ca1f91ae94d949e137b01b
Michaelc322 <michaelc32204@gmail.com>
2023-06-28 14:01:45 -0400

    1.0 Commit

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component, Host, HostListener, OnInit } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+  
+})
+
+
+
+
+export class NavbarComponent {
+
+}
+
