da48e03b447961b784c85614a567934757b1e823
Michaelc322 <michaelc32204@gmail.com>
2026-04-06 18:04:00 -0400

    fix packages

--- src/app/Components/main.components.ts
+++ src/app/Components/main.components.ts
@@ -5,6 +5,7 @@ import { Component, Host, HostListener, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
+  standalone: false,
   styleUrls: ['./main.component.css']
   
 })
